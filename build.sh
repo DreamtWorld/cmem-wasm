3e9da5153f7343132842602ae16ec22339281cdb
llucinat <llucinat@dreamt.world>
2024-11-18 07:23:21 +0100

    Adjust build script to build object file without -DEXPORT_ALL

--- build.sh
+++ build.sh
@@ -15,7 +15,9 @@ else
 	FEATURES="$FEATURES --disable-bulk-memory"
 fi
 
-cpp -P -nostdinc -o out/cmem.wat $CPPFLAGS -DEXPORT_ALL cmem.wat
+cpp -P -nostdinc -o out/cmem.wat $CPPFLAGS cmem.wat
 wat2wasm $FEATURES -ro out/cmem.o out/cmem.wat
-wat2wasm $FEATURES -o out/cmem.wasm out/cmem.wat
+
+cpp -P -nostdinc -o out/cmemdbg.wat $CPPFLAGS -DEXPORT_ALL cmem.wat
+wat2wasm $FEATURES -o out/cmem.wasm out/cmemdbg.wat
 wasm-opt -O3 $OPTFLAGS -o out/cmem.O3.wasm out/cmem.wasm
